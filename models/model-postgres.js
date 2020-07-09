75acfcb39b148a4bc9c2888aebd2d67fc6d1eebe
simonb <simon@brickle.com>
2020-07-09 18:50:16 +0100

    heroku connection model updated

--- models/model-postgres.js
+++ models/model-postgres.js
@@ -1,15 +1,21 @@
-const { Pool, Client } = require('pg')
+const { Client } = require('pg');
 var moment = require('moment');
 var format = require('pg-format');
-// pools will use environment variables
-// for connection information
-const pool = new Pool();
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
 
 	var event_id = process.env['EVENT_ID'];
 
 	function getEvent(req, res, next)
 	{
-		pool.query('SELECT * from strava.events WHERE id = $1', [event_id], (error, results) => {
+		client.query('SELECT * from strava.events WHERE id = $1', [event_id], (error, results) => {
 			if (error) throw error;
 			//
 			// prepare event data for template output
@@ -29,7 +35,7 @@ const pool = new Pool();
 
 
 function getTotalDistance(req, res, next) {
-		pool.query('SELECT SUM(distance) as distance FROM strava.activities', function (error, results) {
+		client.query('SELECT SUM(distance) as distance FROM strava.activities', function (error, results) {
 				if (error) throw error;
 				res.distance = Math.round(results.rows[0].distance/1000);
 				console.log(res.distance);
@@ -39,7 +45,7 @@ function getTotalDistance(req, res, next) {
 
 	function getActivities(req, res, next)
 	{
-		pool.query('SELECT concat(ath.firstname,\' \',substr(ath.lastname,1,1)), act.name,round(act.distance/1000,1),act.type FROM strava.activities act, strava.athletes ath WHERE act.athlete_id = ath.id ORDER BY act.start_date DESC', function (error, results) {
+		client.query('SELECT concat(ath.firstname,\' \',substr(ath.lastname,1,1)), act.name,round(act.distance/1000,1),act.type FROM strava.activities act, strava.athletes ath WHERE act.athlete_id = ath.id ORDER BY act.start_date DESC', function (error, results) {
 				if (error) throw error;
 				res.activities = results.rows;
 				console.log(res.activities);
@@ -49,7 +55,7 @@ function getTotalDistance(req, res, next) {
 
 	function getAthletes(req, res, next)
 	{
-		pool.query('SELECT CONCAT(ath.firstname,\' \', SUBSTR(ath.lastname,1,1)), ath.country, COUNT(act.id), ROUND(SUM(act.distance)/1000,1) FROM strava.athletes ath, strava.activities act GROUP BY ath.id', function (error, results) {
+		client.query('SELECT CONCAT(ath.firstname,\' \', SUBSTR(ath.lastname,1,1)), ath.country, COUNT(act.id), ROUND(SUM(act.distance)/1000,1) FROM strava.athletes ath, strava.activities act GROUP BY ath.id', function (error, results) {
 				if (error) throw error;
 				res.athletes = results.rows;
 				console.log(res.athletes);
@@ -59,7 +65,7 @@ function getTotalDistance(req, res, next) {
 
 	function getActivityTypes(req, res, next)
 	{
-		pool.query('SELECT act.strava_name FROM strava.activity_types act, strava.activity_types_events ae, strava.events e WHERE ae.event_id = e.id and ae.activity_type_id = act.id and e.id = $1', [event_id], function (error, results) {
+		client.query('SELECT act.strava_name FROM strava.activity_types act, strava.activity_types_events ae, strava.events e WHERE ae.event_id = e.id and ae.activity_type_id = act.id and e.id = $1', [event_id], function (error, results) {
 			if (error) throw error;
 
 			// extract friendly name of activities from results and prepare for template output
@@ -86,7 +92,7 @@ function getTotalDistance(req, res, next) {
 		});
 
 		var sql = format('INSERT into strava.activities (id, athlete_id,name, distance, total_elevation_gain, type, start_date) values %L ON CONFLICT (id) DO UPDATE set athlete_id = EXCLUDED.athlete_id, name=EXCLUDED.name, distance=EXCLUDED.distance, total_elevation_gain=EXCLUDED.total_elevation_gain, type=EXCLUDED.type, start_date=EXCLUDED.start_date', insertArray);
-		pool.query(sql, function (error, results) {
+		client.query(sql, function (error, results) {
 				if (error) throw error;
 			});
 
@@ -104,7 +110,7 @@ function getTotalDistance(req, res, next) {
 			req.user._json.sex,
 			req.user._json.profile_medium,
 			req.user._json.profile);
-		pool.query(sql, function (error, results) {
+		client.query(sql, function (error, results) {
 				if (error) throw error;
 				next();
 			});
