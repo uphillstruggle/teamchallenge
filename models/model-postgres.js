b52031ddaedea23e43c4962225938c3178d9ae4f
simonb <simon@brickle.com>
2020-07-09 18:38:14 +0100

    added schema to table names

--- models/model-postgres.js
+++ models/model-postgres.js
@@ -9,7 +9,7 @@ const pool = new Pool()
 
 	function getEvent(req, res, next)
 	{
-		pool.query('SELECT * from events WHERE id = $1', [event_id], (error, results) => {
+		pool.query('SELECT * from strava.events WHERE id = $1', [event_id], (error, results) => {
 			// prepare event data for template output
 			res.event = new Object();
 			res.event.id = results.rows[0].id;
@@ -27,7 +27,7 @@ const pool = new Pool()
 
 
 function getTotalDistance(req, res, next) {
-		pool.query('SELECT SUM(distance) as distance FROM activities', function (error, results) {
+		pool.query('SELECT SUM(distance) as distance FROM strava.activities', function (error, results) {
 				if (error) throw error;
 				res.distance = Math.round(results.rows[0].distance/1000);
 				console.log(res.distance);
@@ -37,7 +37,7 @@ function getTotalDistance(req, res, next) {
 
 	function getActivities(req, res, next)
 	{
-		pool.query('SELECT concat(ath.firstname,\' \',substr(ath.lastname,1,1)), act.name,round(act.distance/1000,1),act.type FROM activities act, athletes ath WHERE act.athlete_id = ath.id ORDER BY act.start_date DESC', function (error, results) {
+		pool.query('SELECT concat(ath.firstname,\' \',substr(ath.lastname,1,1)), act.name,round(act.distance/1000,1),act.type FROM strava.activities act, strava.athletes ath WHERE act.athlete_id = ath.id ORDER BY act.start_date DESC', function (error, results) {
 				if (error) throw error;
 				res.activities = results.rows;
 				console.log(res.activities);
@@ -47,7 +47,7 @@ function getTotalDistance(req, res, next) {
 
 	function getAthletes(req, res, next)
 	{
-		pool.query('SELECT CONCAT(ath.firstname,\' \', SUBSTR(ath.lastname,1,1)), ath.country, COUNT(act.id), ROUND(SUM(act.distance)/1000,1) FROM athletes ath, activities act GROUP BY ath.id', function (error, results) {
+		pool.query('SELECT CONCAT(ath.firstname,\' \', SUBSTR(ath.lastname,1,1)), ath.country, COUNT(act.id), ROUND(SUM(act.distance)/1000,1) FROM strava.athletes ath, strava.activities act GROUP BY ath.id', function (error, results) {
 				if (error) throw error;
 				res.athletes = results.rows;
 				console.log(res.athletes);
@@ -57,7 +57,7 @@ function getTotalDistance(req, res, next) {
 
 	function getActivityTypes(req, res, next)
 	{
-		pool.query('SELECT act.strava_name FROM activity_types act, activity_types_events ae, events e WHERE ae.event_id = e.id and ae.activity_type_id = act.id and e.id = $1', [event_id], function (error, results) {
+		pool.query('SELECT act.strava_name FROM strava.activity_types act, strava.activity_types_events ae, strava.events e WHERE ae.event_id = e.id and ae.activity_type_id = act.id and e.id = $1', [event_id], function (error, results) {
 			if (error) throw error;
 
 			// extract friendly name of activities from results and prepare for template output
@@ -83,7 +83,7 @@ function getTotalDistance(req, res, next) {
 			insertArray.push(values);
 		});
 
-		var sql = format('INSERT into activities (id, athlete_id,name, distance, total_elevation_gain, type, start_date) values %L ON CONFLICT (id) DO UPDATE set athlete_id = EXCLUDED.athlete_id, name=EXCLUDED.name, distance=EXCLUDED.distance, total_elevation_gain=EXCLUDED.total_elevation_gain, type=EXCLUDED.type, start_date=EXCLUDED.start_date', insertArray);
+		var sql = format('INSERT into strava.activities (id, athlete_id,name, distance, total_elevation_gain, type, start_date) values %L ON CONFLICT (id) DO UPDATE set athlete_id = EXCLUDED.athlete_id, name=EXCLUDED.name, distance=EXCLUDED.distance, total_elevation_gain=EXCLUDED.total_elevation_gain, type=EXCLUDED.type, start_date=EXCLUDED.start_date', insertArray);
 		pool.query(sql, function (error, results) {
 				if (error) throw error;
 			});
@@ -93,7 +93,7 @@ function getTotalDistance(req, res, next) {
 	function updateAthlete(req,res,next)
 	{
 		console.log(req.user);
-		var sql = format('INSERT into athletes (id, username, firstname, lastname, country, sex, profile_medium, profile) values (%L, %L, %L, %L, %L, %L, %L, %L) ON CONFLICT (id) DO UPDATE SET username=EXCLUDED.username, firstname=EXCLUDED.firstname, lastname=EXCLUDED.lastname, country=EXCLUDED.country, sex=EXCLUDED.sex, profile_medium=EXCLUDED.profile_medium, profile=EXCLUDED.profile', 
+		var sql = format('INSERT into strava.athletes (id, username, firstname, lastname, country, sex, profile_medium, profile) values (%L, %L, %L, %L, %L, %L, %L, %L) ON CONFLICT (id) DO UPDATE SET username=EXCLUDED.username, firstname=EXCLUDED.firstname, lastname=EXCLUDED.lastname, country=EXCLUDED.country, sex=EXCLUDED.sex, profile_medium=EXCLUDED.profile_medium, profile=EXCLUDED.profile', 
 			req.user._json.id,
 			req.user._json.username,
 			req.user._json.firstname,
