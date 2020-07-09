0a19051b9d80efee4b52d7cf6dbbd3199bc03be5
simonb <simon@brickle.com>
2020-07-09 22:31:24 +0100

    webhook subscription added

--- routes/webhook.js
+++ routes/webhook.js
@@ -8,4 +8,25 @@ router.post('/', (req, res) => {
 	res.status(200).send('EVENT_RECEIVED');
 });
 
+router.get('/', (req, res) => {
+  const VERIFY_TOKEN = process.env['VERIFY_TOKEN'];
+	
+  // Parses the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Verifies that the mode and token sent are valid
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.json({"hub.challenge":challenge});
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      console.log('Message received', mode, token);
+      res.sendStatus(403);
+    }
+  }
+});
 module.exports = router;
